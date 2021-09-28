bc4ba185b206b4a642be5f146f008a3fa5582c6b
Francisco Javier Rubio <fxrubio@gmail.com>
2021-09-28 20:12:58 +0100

    help files added

--- R/DTP.R
+++ R/DTP.R
@@ -1,132 +1,168 @@
-# Probability Density Function
-
-ddtp <- function (x, mu, par1, par2, delta1, delta2, f, param = "tp", 
-                  log = FALSE) 
+#' Probability Density Function for DTP distributions
+#' @param x: vector of quantiles.
+#' @param p: vector of probabilities.
+#' @param n: number of observations. If length(n) > 1, the length is taken to be the number required.
+#' @param mu: location parameter.
+#' @param par1: scale parameter 1.
+#' @param par2: scale parameter 2.
+#' @param delta1: shape parameter 1.
+#' @param delta2: shape parameter 2.
+#' @param F, qF, rF, f: distribution function, quantile function, random function and density function associated of a symmetric random variable.
+#' @param param: parameterisations used.
+#' @param log, log.p: logical; if TRUE, probabilities p are given as log(p).
+#' @return
+#' @export
+ddtp <- function (x, mu, par1, par2, delta1, delta2, f, param = "tp",
+                  log = FALSE)
 {
   if (param == "tp") {
     sigma1 = par1
     sigma2 = par2
-    logeps <- log(sigma1) + f(0, delta2, log = T) - log(sigma1 * 
+    logeps <- log(sigma1) + f(0, delta2, log = T) - log(sigma1 *
                                                           f(0, delta2, log = F) + sigma2 * f(0, delta1, log = F))
-    log1eps <- log(sigma2) + f(0, delta1, log = T) - log(sigma1 * 
+    log1eps <- log(sigma2) + f(0, delta1, log = T) - log(sigma1 *
                                                            f(0, delta2, log = F) + sigma2 * f(0, delta1, log = F))
-    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0, 
-           logPDF <- log(2) + ifelse(x < mu, logeps + f((x - 
-                                                           mu)/sigma1, delta1, log = T) - log(sigma1), 
-                                     log1eps + f((x - mu)/sigma2, delta2, log = T) - 
+    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0,
+           logPDF <- log(2) + ifelse(x < mu, logeps + f((x -
+                                                           mu)/sigma1, delta1, log = T) - log(sigma1),
+                                     log1eps + f((x - mu)/sigma2, delta2, log = T) -
                                        log(sigma2)), logPDF <- "invalid arguments: par1 or/and par2 or/and delta1 or/and delta2 out of range in the parametrization tp")
   }
   if (param == "eps") {
     sigma1 = par1 * (1 + par2)
     sigma2 = par1 * (1 - par2)
-    logeps <- log(sigma1) + f(0, delta2, log = T) - log(sigma1 * 
+    logeps <- log(sigma1) + f(0, delta2, log = T) - log(sigma1 *
                                                           f(0, delta2, log = F) + sigma2 * f(0, delta1, log = F))
-    log1eps <- log(sigma2) + f(0, delta1, log = T) - log(sigma1 * 
+    log1eps <- log(sigma2) + f(0, delta1, log = T) - log(sigma1 *
                                                            f(0, delta2, log = F) + sigma2 * f(0, delta1, log = F))
-    ifelse(par1 > 0 & abs(par2) < 1 & delta1 > 0 & delta2 > 
-             0, logPDF <- log(2) + ifelse(x < mu, logeps + f((x - 
-                                                                mu)/sigma1, delta1, log = T) - log(sigma1), log1eps + 
-                                            f((x - mu)/sigma2, delta2, log = T) - log(sigma2)), 
+    ifelse(par1 > 0 & abs(par2) < 1 & delta1 > 0 & delta2 >
+             0, logPDF <- log(2) + ifelse(x < mu, logeps + f((x -
+                                                                mu)/sigma1, delta1, log = T) - log(sigma1), log1eps +
+                                            f((x - mu)/sigma2, delta2, log = T) - log(sigma2)),
            logPDF <- "invalid arguments: par1 or/and delta1 or/and delta2 is/are not positive or/and abs(par2) out of range in the parametrization eps")
   }
   if (param == "isf") {
     sigma1 = par1 * par2
     sigma2 = par1/par2
-    logeps <- log(sigma1) + f(0, delta2, log = T) - log(sigma1 * 
+    logeps <- log(sigma1) + f(0, delta2, log = T) - log(sigma1 *
                                                           f(0, delta2, log = F) + sigma2 * f(0, delta1, log = F))
-    log1eps <- log(sigma2) + f(0, delta1, log = T) - log(sigma1 * 
+    log1eps <- log(sigma2) + f(0, delta1, log = T) - log(sigma1 *
                                                            f(0, delta2, log = F) + sigma2 * f(0, delta1, log = F))
-    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0, 
-           logPDF <- log(2) + ifelse(x < mu, logeps + f((x - 
-                                                           mu)/sigma1, delta1, log = T) - log(sigma1), 
-                                     log1eps + f((x - mu)/sigma2, delta2, log = T) - 
+    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0,
+           logPDF <- log(2) + ifelse(x < mu, logeps + f((x -
+                                                           mu)/sigma1, delta1, log = T) - log(sigma1),
+                                     log1eps + f((x - mu)/sigma2, delta2, log = T) -
                                        log(sigma2)), logPDF <- "invalid arguments: par1 or/and par2 or/and delta1 or/and delta2 out of range in the parametrization isf")
   }
-  ifelse(is.numeric(logPDF), ifelse(log, return(logPDF), return(exp(logPDF))), 
+  ifelse(is.numeric(logPDF), ifelse(log, return(logPDF), return(exp(logPDF))),
          logPDF)
 }
 
 
-# Cumulative Probability Function
-
-pdtp <- function (x, mu, par1, par2, delta1, delta2, F, f, param = "tp", 
-                  log.p = FALSE) 
+#' Cumulative Probability Function for DTP distributions
+#' @param x: vector of quantiles.
+#' @param p: vector of probabilities.
+#' @param n: number of observations. If length(n) > 1, the length is taken to be the number required.
+#' @param mu: location parameter.
+#' @param par1: scale parameter 1.
+#' @param par2: scale parameter 2.
+#' @param delta1: shape parameter 1.
+#' @param delta2: shape parameter 2.
+#' @param F, qF, rF, f: distribution function, quantile function, random function and density function associated of a symmetric random variable.
+#' @param param: parameterisations used.
+#' @param log, log.p: logical; if TRUE, probabilities p are given as log(p).
+#' @return
+#' @export
+pdtp <- function (x, mu, par1, par2, delta1, delta2, F, f, param = "tp",
+                  log.p = FALSE)
 {
   if (param == "tp") {
     sigma1 = par1
     sigma2 = par2
-    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 * 
-                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1, 
+    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 *
+                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1,
                                                                                         log = FALSE))
-    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0, 
-           CDF <- ifelse(x < mu, 2 * eps * F((x - mu)/sigma1, 
-                                             delta1, log.p = FALSE), eps + (1 - eps) * (2 * 
-                                                                                          F((x - mu)/sigma2, delta2, log.p = FALSE) - 
+    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0,
+           CDF <- ifelse(x < mu, 2 * eps * F((x - mu)/sigma1,
+                                             delta1, log.p = FALSE), eps + (1 - eps) * (2 *
+                                                                                          F((x - mu)/sigma2, delta2, log.p = FALSE) -
                                                                                           1)), CDF <- "invalid arguments: par1 or/and par2 or/and delta is/are no positive in the parametrization tp")
   }
   if (param == "eps") {
     sigma1 = par1 * (1 + par2)
     sigma2 = par1 * (1 - par2)
-    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 * 
-                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1, 
+    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 *
+                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1,
                                                                                         log = FALSE))
-    ifelse(par1 > 0 & abs(par2) < 1 & delta1 > 0 & delta2 > 
-             0, CDF <- ifelse(x < mu, 2 * eps * F((x - mu)/sigma1, 
-                                                  delta1, log.p = FALSE), eps + (1 - eps) * (2 * F((x - 
+    ifelse(par1 > 0 & abs(par2) < 1 & delta1 > 0 & delta2 >
+             0, CDF <- ifelse(x < mu, 2 * eps * F((x - mu)/sigma1,
+                                                  delta1, log.p = FALSE), eps + (1 - eps) * (2 * F((x -
                                                                                                       mu)/sigma2, delta2, log.p = FALSE) - 1)), CDF <- "invalid arguments: par1 is not positive or/and abs(par2) is not less than 1 in the parametrization eps")
   }
   if (param == "isf") {
     sigma1 = par1 * par2
     sigma2 = par1/par2
-    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 * 
-                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1, 
+    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 *
+                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1,
                                                                                         log = FALSE))
-    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0, 
-           CDF <- ifelse(x < mu, 2 * eps * F((x - mu)/sigma1, 
-                                             delta1, log.p = FALSE), eps + (1 - eps) * (2 * 
-                                                                                          F((x - mu)/sigma2, delta2, log.p = FALSE) - 
+    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0,
+           CDF <- ifelse(x < mu, 2 * eps * F((x - mu)/sigma1,
+                                             delta1, log.p = FALSE), eps + (1 - eps) * (2 *
+                                                                                          F((x - mu)/sigma2, delta2, log.p = FALSE) -
                                                                                           1)), CDF <- "invalid arguments: par1 or/and par2 is/are not positive in the parametrization isf")
   }
-  ifelse(is.numeric(CDF), ifelse(log.p, return(log(CDF)), 
+  ifelse(is.numeric(CDF), ifelse(log.p, return(log(CDF)),
                                  return(CDF)), CDF)
 }
 
 
-# Random Number Generation
-
-rdtp <- function (n, mu, par1, par2, delta1, delta2, rF, f, param = "tp") 
+#' Random Number Generation for DTP distributions
+#' @param x: vector of quantiles.
+#' @param p: vector of probabilities.
+#' @param n: number of observations. If length(n) > 1, the length is taken to be the number required.
+#' @param mu: location parameter.
+#' @param par1: scale parameter 1.
+#' @param par2: scale parameter 2.
+#' @param delta1: shape parameter 1.
+#' @param delta2: shape parameter 2.
+#' @param F, qF, rF, f: distribution function, quantile function, random function and density function associated of a symmetric random variable.
+#' @param param: parameterisations used.
+#' @param log, log.p: logical; if TRUE, probabilities p are given as log(p).
+#' @return
+#' @export
+rdtp <- function (n, mu, par1, par2, delta1, delta2, rF, f, param = "tp")
 {
   if (param == "tp") {
     sigma1 = par1
     sigma2 = par2
-    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 * 
-                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1, 
+    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 *
+                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1,
                                                                                         log = FALSE))
-    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0, 
-           sample <- ifelse(runif(n) < eps, mu - sigma1 * abs(rF(n, 
-                                                                 delta1)), mu + sigma2 * abs(rF(n, delta2))), 
+    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0,
+           sample <- ifelse(runif(n) < eps, mu - sigma1 * abs(rF(n,
+                                                                 delta1)), mu + sigma2 * abs(rF(n, delta2))),
            sample <- "invalid arguments: par1 or/and par2 or/and delta is/are no positive in the parametrization tp")
   }
   if (param == "eps") {
     sigma1 = par1 * (1 + par2)
     sigma2 = par1 * (1 - par2)
-    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 * 
-                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1, 
+    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 *
+                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1,
                                                                                         log = FALSE))
-    ifelse(par1 > 0 & abs(par2) < 1 & delta1 > 0 & delta2 > 
-             0, sample <- ifelse(runif(n) < eps, mu - sigma1 * 
-                                   abs(rF(n, delta1)), mu + sigma2 * abs(rF(n, delta2))), 
+    ifelse(par1 > 0 & abs(par2) < 1 & delta1 > 0 & delta2 >
+             0, sample <- ifelse(runif(n) < eps, mu - sigma1 *
+                                   abs(rF(n, delta1)), mu + sigma2 * abs(rF(n, delta2))),
            sample <- "invalid arguments: par1 is not positive or/and abs(par2) is not less than 1 in the parametrization eps")
   }
   if (param == "isf") {
     sigma1 = par1 * par2
     sigma2 = par1/par2
-    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 * 
-                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1, 
+    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 *
+                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1,
                                                                                         log = FALSE))
-    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0, 
-           sample <- ifelse(runif(n) < eps, mu - sigma1 * abs(rF(n, 
-                                                                 delta1)), mu + sigma2 * abs(rF(n, delta2))), 
+    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0,
+           sample <- ifelse(runif(n) < eps, mu - sigma1 * abs(rF(n,
+                                                                 delta1)), mu + sigma2 * abs(rF(n, delta2))),
            sample <- "invalid arguments: par1 or/and par2 is/are not positive in the parametrization isf")
   }
   return(sample)
@@ -134,41 +170,53 @@ rdtp <- function (n, mu, par1, par2, delta1, delta2, rF, f, param = "tp")
 
 
 
-# Quantile Function
-
-qdtp <- function (p, mu, par1, par2, delta1, delta2, qF, f, param = "tp") 
+#' Quantile Function for DTP distributions
+#' @param x: vector of quantiles.
+#' @param p: vector of probabilities.
+#' @param n: number of observations. If length(n) > 1, the length is taken to be the number required.
+#' @param mu: location parameter.
+#' @param par1: scale parameter 1.
+#' @param par2: scale parameter 2.
+#' @param delta1: shape parameter 1.
+#' @param delta2: shape parameter 2.
+#' @param F, qF, rF, f: distribution function, quantile function, random function and density function associated of a symmetric random variable.
+#' @param param: parameterisations used.
+#' @param log, log.p: logical; if TRUE, probabilities p are given as log(p).
+#' @return
+#' @export
+qdtp <- function (p, mu, par1, par2, delta1, delta2, qF, f, param = "tp")
 {
   if (param == "tp") {
     sigma1 = par1
     sigma2 = par2
-    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 * 
-                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1, 
+    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 *
+                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1,
                                                                                         log = FALSE))
-    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0, 
-           Q <- ifelse(p < eps, mu + sigma1 * qF(p/(2 * eps), 
-                                                 delta1), mu + sigma2 * qF(0.5 * (1 + (p - eps)/(1 - 
+    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0,
+           Q <- ifelse(p < eps, mu + sigma1 * qF(p/(2 * eps),
+                                                 delta1), mu + sigma2 * qF(0.5 * (1 + (p - eps)/(1 -
                                                                                                    eps)), delta2)), Q <- "invalid arguments: par1 or/and par2 or/and delta is/are no positive in the parametrization tp")
   }
   if (param == "eps") {
     sigma1 = par1 * (1 + par2)
     sigma2 = par1 * (1 - par2)
-    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 * 
-                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1, 
+    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 *
+                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1,
                                                                                         log = FALSE))
-    ifelse(par1 > 0 & abs(par2) < 1 & delta1 > 0 & delta2 > 
-             0, Q <- ifelse(p < eps, mu + sigma1 * qF(p/(2 * 
-                                                           eps), delta1), mu + sigma2 * qF(0.5 * (1 + (p - 
+    ifelse(par1 > 0 & abs(par2) < 1 & delta1 > 0 & delta2 >
+             0, Q <- ifelse(p < eps, mu + sigma1 * qF(p/(2 *
+                                                           eps), delta1), mu + sigma2 * qF(0.5 * (1 + (p -
                                                                                                          eps)/(1 - eps)), delta2)), Q <- "invalid arguments: par1 is not positive or/and abs(par2) is not less than 1 in the parametrization eps")
   }
   if (param == "isf") {
     sigma1 = par1 * par2
     sigma2 = par1/par2
-    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 * 
-                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1, 
+    eps <- sigma1 * f(0, delta2, log = FALSE)/(sigma1 *
+                                                 f(0, delta2, log = FALSE) + sigma2 * f(0, delta1,
                                                                                         log = FALSE))
-    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0, 
-           Q <- ifelse(p < eps, mu + sigma1 * qF(p/(2 * eps), 
-                                                 delta1), mu + sigma2 * qF(0.5 * (1 + (p - eps)/(1 - 
+    ifelse(par1 > 0 & par2 > 0 & delta1 > 0 & delta2 > 0,
+           Q <- ifelse(p < eps, mu + sigma1 * qF(p/(2 * eps),
+                                                 delta1), mu + sigma2 * qF(0.5 * (1 + (p - eps)/(1 -
                                                                                                    eps)), delta2)), Q <- "invalid arguments: par1 or/and par2 is/are not positive in the parametrization isf")
   }
   return(Q)
